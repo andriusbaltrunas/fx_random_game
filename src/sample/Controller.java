f6b283b135a5ab265cfe9aafa9c6150598104920
andriusbaltrunas <andriusbaltrunas@yahoo.com>
2018-03-27 20:57:12 +0300

    Mantas kodas

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -5,6 +5,8 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
+import java.util.Random;
+
 public class Controller {
     @FXML
     private TextField inputNumber;
@@ -13,6 +15,13 @@ public class Controller {
     private Label answerLabel;
 
     public void tryMe (ActionEvent event){
-
+        Random rnd = new Random();
+        int rndNumber = rnd.nextInt(10) + 1;
+        int guessNumber = Integer.valueOf(inputNumber.getText());
+        if (rndNumber == guessNumber) {
+            System.out.println("Jus atspejote, tai buvo " + guessNumber);
+        }else {
+            System.out.println("Spekite dar karta");
+        }
     }
 }
