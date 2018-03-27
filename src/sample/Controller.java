d2107d9af36e4235d8a261641db1615f81952021
andriusbaltrunas <andriusbaltrunas@yahoo.com>
2018-03-27 23:32:50 +0300

    add method to triger action from enter press

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -5,6 +5,8 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 
 import java.util.InputMismatchException;
 import java.util.Random;
@@ -17,20 +19,26 @@ public class Controller {
     private Label answerLabel;
 
     public void tryMe(ActionEvent event) {
+        allLogic();
+    }
+
+    public void enterClicked(KeyEvent event) {
+        if (event.getCode() == KeyCode.ENTER && !inputNumber.getText().equals("")) {
+            allLogic();
+        }
+    }
+
+
+    private void allLogic() {
         Random rnd = new Random();
         int rndNumber = rnd.nextInt(10) + 1;
         try {
-
-
             int guessNumber = Integer.valueOf(inputNumber.getText());
             inputNumber.setText("");
             if (rndNumber == guessNumber) {
                 answerLabel.setText("Jusu spejimas buvo teisingas, generuotas skaicius:" + rndNumber + " Jusu spejimas buvo: " + guessNumber);
-
-
             } else {
                 answerLabel.setText("Neatspejote, generuotas skaicius: " + rndNumber + " Jusu skaicius: " + guessNumber);
-
             }
         } catch (NumberFormatException e) {
             inputNumber.setText("");
