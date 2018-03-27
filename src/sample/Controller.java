96dedbc3000cfa85192b144c4c93885375ecea52
andriusbaltrunas <andriusbaltrunas@yahoo.com>
2018-03-27 20:49:15 +0300

    Aurelija. Pridejau mygtuka

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,9 +1,18 @@
 package sample;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 public class Controller {
     @FXML
     private TextField inputNumber;
+
+    @FXML
+    private Label answerLabel;
+
+    public void tryMe (ActionEvent event){
+
+    }
 }
